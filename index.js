0495fd61637809c50345168515f85c920e539a6c
Damian-luis <damian.luis.porta@gmail.com>
2022-07-03 23:42:41 -0300

    filter  data

--- index.js
+++ index.js
@@ -10,13 +10,33 @@ app.listen(port,()=>{
     console.log("escuchando en el puerto 3000")
 })
 
+
+
+
 app.get("/info",(req, res)=>{res.send("aqui todo bien")})
 app.get("/users",async(req, res)=>{
-
     const data = await axios.get("https://gorest.co.in/public/v2/users")
+    
     res.send(data.data)
 
 })
+app.get("/users/status/:status",async(req, res)=>{
+    
+    const status=req.params.status;
+    
+    const data = await axios.get("https://gorest.co.in/public/v2/users").then(e=>{ return e.data})
+    const result=data.filter(e=>{return e.status==status})
 
+    res.send(result)
+})
 
+app.get("/users/gender/:gender",async(req, res)=>{
+    
+    const gender=req.params.gender;
+    
+    const data = await axios.get("https://gorest.co.in/public/v2/users").then(e=>{ return e.data})
+    const result=data.filter(e=>{return e.gender==gender})
+
+    res.send(result)
+})
 
